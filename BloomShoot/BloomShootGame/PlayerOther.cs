6ea5250bb41a48d2351614405c1c26dc1adaf5d5
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2025-01-10 20:35:13 +0100

    Přidání draw podprogramu pro druhého hráče

--- BloomShoot/BloomShootGame/PlayerOther.cs
+++ BloomShoot/BloomShootGame/PlayerOther.cs
@@ -11,11 +11,31 @@ public class PlayerOther
     public Vector2 Position => _position;
     
     private List<Bullet> _bullets;
+    private Texture2D _texture;
     
     public PlayerOther(GraphicsDevice graphicsDevice, Vector2 position)
     {
         _position = new Vector2();
+        
+        // TODO: vyměnit za texturu
+        _texture = new Texture2D(graphicsDevice, 35, 35);
+        Color[] colors = new Color[35 * 35];
+
+        for (int i = 0; i < 35; i++)
+        {
+            for (int j = 0; j < 35; j++)
+            {
+                colors[i + j * 35] = Color.Orange;
+            }
+        }
+        
+        _texture.SetData(colors);
     }
 
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, _position, Color.White);
+    }
+    
     public void SetPosition(Vector2 position) { _position = position; }
 }
