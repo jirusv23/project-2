2aff72191169dfaa17e6bf6f81200d2f158f1c39
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2025-01-13 12:14:46 +0100

    Přepsání Program souborů z toplevel

--- BloomShoot/BloomShootServer/Program.cs
+++ BloomShoot/BloomShootServer/Program.cs
@@ -1,2 +1,8 @@
-﻿using var game = new BloomShootServer.Game1();
-game.Run();
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        using var game = new BloomShootServer.Game1();
+        game.Run(); 
+    }
+}
