dd088f6d0f2f7e311ae6057f276dcc0f1e91525a
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2025-01-13 12:17:32 +0100

    Přidání menu souboru

---
+++ BloomShoot/BloomShootGame/Menu.cs
@@ -0,0 +1,73 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace BloomShootGame;
+
+public class BloomShootMenuProgram : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private PlayerLocal _playerLocal;
+    private PlayerOther _playerOther;
+    private SpriteFont _font;
+    
+    private Vector2 _middleOfScreen;
+
+    public BloomShootMenuProgram()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+
+        
+        _graphics.PreferredBackBufferWidth = 1920;
+        _graphics.PreferredBackBufferHeight = 1080;
+        _graphics.IsFullScreen = true;
+        
+        _graphics.ApplyChanges();
+    }
+
+    protected override void Initialize()
+    {
+        _middleOfScreen = new Vector2(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+        
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _font = Content.Load<SpriteFont>("Font1");
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        var KeyboardState = Keyboard.GetState();
+        
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+            Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        base.Update(gameTime);
+
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+
+        _spriteBatch.Begin();
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+
+    protected override void UnloadContent()
+    {
+        // TODO: zde zavřít klienta pro multiplayer
+    }
+}
