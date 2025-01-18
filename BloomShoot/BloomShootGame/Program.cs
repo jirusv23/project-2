0939fb137322b7647a621ce05e39ba156248d2f9
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2025-01-18 11:50:23 +0100

    okay ... you do you
    (Něco si přepsalo zápis)

--- BloomShoot/BloomShootGame/Program.cs
+++ BloomShoot/BloomShootGame/Program.cs
@@ -13,15 +13,15 @@ internal class Program
             // Po ukončení hry se dle výběru pokračuje
             switch (menu.SelectedGameSettings)
             {
-                case 0:
-                    using (var game = new BloomShootGameProgram())
+                case 1:
+                    using (var game = new BloomShootGameSinglePlayerProgram())
                         game.Run();
                     break;
-                case 1:
+                case 0:
                     using (var gameMenu = new MultiplayerMenu())
                     {
                         gameMenu.Run();
-                        using (var game = new BloomShootGameSinglePlayerProgram())
+                        using (var game = new BloomShootGameProgram())
                             game.Run();
                     }
                     break;
